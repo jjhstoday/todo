977d0605bd22db30e238f1786c36d86f8e0b0f84
jjhstoday <jjh.s.today@gmail.com>
2021-01-19 19:59:28 +0900

    docs: develop code 210114.ver

--- todos-v3.0/js/app.js
+++ todos-v3.0/js/app.js
@@ -1,38 +1,24 @@
 let todos = [];
-let todosNav;
+let navState = 'all';
 
 // DOM nodes
-const $todos = document.querySelector('.todos');
+const $todoList = document.querySelector('.todos');
 const $inputTodo = document.querySelector('.input-todo');
 const $completeAll = document.querySelector('.complete-all');
 const $clearCompleted = document.querySelector('.clear-completed');
 const $completedCount = document.querySelector('.completed-todos');
 const $activeCount = document.querySelector('.active-todos');
 const $nav = document.querySelector('.nav');
-const $allNav = document.getElementById('all');
-const $activeNav = document.getElementById('active');
-const $completedNav = document.getElementById('completed');
 
-
-// 브라우저 렌더링
 const render = () => {
-  if ($allNav.classList.contains('active')) todosNav = todos;
-  else if ($activeNav.classList.contains('active')) todosNav = todos.filter(todo => !todo.completed);
-  else todosNav = todos.filter(todo => todo.completed);
-
-  // innerHTML 방법
-  // $todos.innerHTML = todosNav.map(({ id, content, completed }) => 
-  //   `<li id="${id}" class="todo-item">
-  //     <input id="ck-${id}" class="checkbox" type="checkbox" ${completed ? 'checked' : ''}>
-  //     <label for="ck-${id}">${content}</label>
-  //     <i class="remove-todo far fa-times-circle"></i>
-  //   </li>`).join('');
-
+  const activeTodos = todos.filter(todo => !todo.completed);
+  const completedTodos = todos.filter(todo => todo.completed);
   
-  // DocumentFragment 노드 생성 방법
+  let todosNav = navState === 'all' ? todos : navState === 'active' ? activeTodos : completedTodos;
+  
   const $fragment = document.createDocumentFragment();
 
-  $todos.textContent = '';
+  $todoList.textContent = '';
   todosNav.forEach(({ id, content, completed }) => {
     const $li = document.createElement('li');
     $li.setAttribute('id', id);
@@ -56,22 +42,20 @@ const render = () => {
     $li.appendChild($input);
     $li.appendChild($label);
     $li.appendChild($i);
-
     $fragment.appendChild($li);
-  });
-  $todos.appendChild($fragment);
-  
-  $completedCount.textContent = todos.filter(todo => todo.completed).length;
-  $activeCount.textContent = todos.filter(todo => !todo.completed).length;
-  
-  // if (!todos.length) $completeAll.firstElementChild.checked = false;
-  
-  if (todos.map(todo => todo.completed ? 1 : 0).includes(0)) $completeAll.firstElementChild.checked = false;
-  else $completeAll.firstElementChild.checked = true;
+  })
+  $todoList.appendChild($fragment);
+
+  $completedCount.textContent = completedTodos.length;
+  $activeCount.textContent = activeTodos.length;
+
+  $completeAll.firstElementChild.checked = completedTodos.length === todos.length;
+
+  if (!todos.length) $completeAll.firstElementChild.checked = false;
 };
 
-// 서버 데이터 패치
 const fetchTodos = () => {
+  //TODO: 서버로부터 데이터 취득 예정
   todos = [
     { id: 1, content: 'HTML', completed: false },
     { id: 2, content: 'CSS', completed: true },
@@ -83,16 +67,21 @@ const fetchTodos = () => {
 };
 
 const addTodo = (() => {
-  const generateId = () => todos.length ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+  const generateId = () => todos.length ? Math.max(...(todos.map(todo => todo.id))) + 1 : 1;
   
   return content => {
     todos = [{ id: generateId(), content, completed: false }, ...todos];
     render();
-  };
+  }
 })();
 
-const toggleTodo = id => {
-  todos = todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo);
+const checkToggle = id => {
+  todos = todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo);
+  render();
+};
+
+const checkAllToggle = checked => {
+  todos = todos.map(todo => ({ ...todo, completed: checked }));
   render();
 };
 
@@ -101,45 +90,37 @@ const removeTodo = id => {
   render();
 };
 
-const toggleAllTodo = checked => {
-  todos = todos.map(todo => ({...todo, completed: checked}));
-  render();
-};
-
-const removeCompleted = () => {
+const removeAllCompleted = () => {
   todos = todos.filter(todo => !todo.completed);
   render();
 };
 
-const seperateNav = target => {
+const navToggle = target => {
   [...$nav.children].forEach(nav => nav.classList.toggle('active', nav === target));
+  navState = target.id;
   render();
 };
 
 document.addEventListener('DOMContentLoaded', fetchTodos);
 
 $inputTodo.onkeyup = e => {
-  const content = e.target.value;
+  const content = e.target.value
   if (e.key !== 'Enter' || content === '') return;
 
   addTodo(content);
   e.target.value = '';
 };
 
-$todos.onchange = e => {
-  if (!e.target.classList.contains('checkbox')) return;
+$todoList.onchange = e => checkToggle(+e.target.parentNode.id);
 
-  toggleTodo(+e.target.parentNode.id);
-};
+$completeAll.onchange = e => checkAllToggle(e.target.checked);
 
-$todos.onclick = e => {
-  if (!e.target.classList.contains('remove-todo')) return;
+$todoList.onclick = e => {
+  if (!e.target.matches('.todos > li > .remove-todo')) return;
 
   removeTodo(+e.target.parentNode.id);
 };
 
-$completeAll.onchange = e => toggleAllTodo(e.target.checked);
+$clearCompleted.onclick = () => removeAllCompleted();
 
-$clearCompleted.onclick = e => removeCompleted();
-
-$nav.onclick = e => seperateNav(e.target);
+$nav.onclick = e => navToggle(e.target)
