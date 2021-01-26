93fbb227f800f1e90c20d5f301492c88f15b9082
jjhstoday <jjh.s.today@gmail.com>
2021-01-26 22:12:44 +0900

    docs: async/await

---
+++ todos-v3.0/public/js/4.async-await.js
@@ -0,0 +1,188 @@
+let todos = [];
+let navState = 'all';
+
+// 서버 통신 using async/await
+const async = (() => {
+    return {
+      async get(url, cb) {
+        const response = await fetch(url);
+        const data = await response.json();
+        const todos = await cb(data);
+        render(todos);
+      },
+  
+      async post(url, cb, payload) {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'content-Type': 'application/json'
+          },
+          body: JSON.stringify(payload)
+        })
+        const data = await response.json();
+        const todos = await cb(data);
+        render(todos);
+      },
+  
+      async patch(url, cb, payload) {
+        const response = await fetch(url, {
+          method: 'PATCH',
+          headers: {
+            'content-Type': 'application/json'
+          },
+          body: JSON.stringify(payload)
+        })
+        const data = await response.json();
+        const todos = await cb(data);
+        render(todos);
+      },
+  
+      async delete(url, cb) {
+        const response = await fetch(url, {
+          method: 'DELETE'
+        })
+        const data = await response.json();
+        const todos = await cb(data);
+        render(todos);
+      },
+    }
+})();
+
+// DOM nodes
+const $todoList = document.querySelector('.todos');
+const $inputTodo = document.querySelector('.input-todo');
+const $completeAll = document.querySelector('.complete-all');
+const $clearCompleted = document.querySelector('.clear-completed');
+const $completedCount = document.querySelector('.completed-todos');
+const $activeCount = document.querySelector('.active-todos');
+const $nav = document.querySelector('.nav');
+
+const render = () => {
+  const activeTodos = todos.filter(todo => !todo.completed);
+  const completedTodos = todos.filter(todo => todo.completed);
+  
+  let todosNav = navState === 'all' ? todos : navState === 'active' ? activeTodos : completedTodos;
+  
+  const $fragment = document.createDocumentFragment();
+
+  $todoList.textContent = '';
+  todosNav.forEach(({ id, content, completed }) => {
+    const $li = document.createElement('li');
+    $li.setAttribute('id', id);
+    $li.setAttribute('class', 'todo-item');
+
+    const $input = document.createElement('input');
+    $input.setAttribute('id', `ck-${id}`);
+    $input.setAttribute('class', 'checkbox');
+    $input.setAttribute('type', 'checkbox');
+    if (completed) $input.setAttribute('checked', 'checked');
+    else $input.removeAttribute('checked');
+
+    const $label = document.createElement('label');
+    $label.setAttribute('for', `ck-${id}`);
+    const $textNode = document.createTextNode(content);
+    $label.appendChild($textNode);
+
+    const $i = document.createElement('i');
+    $i.setAttribute('class', 'remove-todo far fa-times-circle');
+
+    $li.appendChild($input);
+    $li.appendChild($label);
+    $li.appendChild($i);
+    $fragment.appendChild($li);
+  })
+  $todoList.appendChild($fragment);
+
+  $completedCount.textContent = completedTodos.length;
+  $activeCount.textContent = activeTodos.length;
+
+  $completeAll.firstElementChild.checked = completedTodos.length === todos.length;
+
+  if (!todos.length) $completeAll.firstElementChild.checked = false;
+};
+
+const fetchTodos = () => {
+  const cb = data => todos = [...data].sort((todo1, todo2) => todo2.id - todo1.id);
+
+  async.get('/todos', cb)
+  .catch(err => console.log(err));
+};
+
+const addTodo = (() => {
+  const generateId = () => todos.length ? Math.max(...(todos.map(todo => todo.id))) + 1 : 1;
+
+  return content => {
+    const cb = data => todos = [data, ...todos];
+    const payload = { id: generateId(), content, completed: false };
+
+    async.post('/todos', cb, payload)
+    .catch(err => console.log(err));
+  }
+})();
+
+const checkToggle = id => {
+  const todo = todos.find(todo => todo.id === id);
+  const cb = data => todos = todos.map(todo => todo.id === data.id ? data : todo);
+  const payload = { ...todo, completed: !todo.completed };
+
+  async.patch(`/todos/${id}`, cb, payload)
+  .catch(err => console.log(err));
+};
+
+const checkAllToggle = checked => {
+  todos = todos.map(todo => ({ ...todo, completed: checked }));
+  const cb = data => todos = todos.map(todo => ({ ...todo, data }));
+  const payload = { completed: checked };
+
+  todos.forEach(todo => {
+    async.patch(`/todos/${todo.id}`, cb, payload)
+    .catch(err => console.log(err));
+  })
+};
+
+const removeTodo = id => {
+  const cb = () => todos = todos.filter(todo => todo.id !== id);
+
+  async.delete(`/todos/${id}`, cb)
+  .catch(err => console.log(err));
+};
+
+const removeAllCompleted = () => {
+  const completedTodos = todos.filter(todo => todo.completed);
+  const cb = () => todos = todos.filter(todo => !todo.completed);
+
+  completedTodos.forEach(todo => {
+    async.delete(`/todos/${todo.id}`, cb)
+    .catch(err => console.log(err));
+  })
+};
+
+const navToggle = target => {
+  [...$nav.children].forEach(nav => nav.classList.toggle('active', nav === target));
+  navState = target.id;
+  render();
+};
+
+document.addEventListener('DOMContentLoaded', fetchTodos);
+
+$inputTodo.onkeyup = e => {
+  const content = e.target.value
+  if (e.key !== 'Enter' || content === '') return;
+
+  addTodo(content);
+  e.target.value = '';
+};
+
+$todoList.onchange = e => checkToggle(+e.target.parentNode.id);
+
+$completeAll.onchange = e => checkAllToggle(e.target.checked);
+
+$todoList.onclick = e => {
+  if (!e.target.matches('.todos > li > .remove-todo')) return;
+
+  removeTodo(+e.target.parentNode.id);
+};
+
+$clearCompleted.onclick = () => removeAllCompleted();
+
+$nav.onclick = e => navToggle(e.target);
